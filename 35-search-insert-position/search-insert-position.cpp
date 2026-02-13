06f620309a9e821dd3d65d25677314939fb0e63e
Samprity Haque <162048249+httprity@users.noreply.github.com>
2026-02-13 20:53:51 +0600

    Simplified one updated
    Refactored searchInsert method to simplify logic for finding the insert position.

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -19,4 +19,27 @@ public:
         return (int)(it - diff.begin());
 
     }
+}; 
+
+//idk why did i take the hardest approach :(( here's the easier one
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int idx = 0;
+        int min_value = 0;
+        vector<int> diff(nums.size(), INT_MAX);
+        for (int i = 0; i < nums.size(); i++) {
+            if (target == nums[i]) {
+                idx = i;
+                return idx;
+            } else {
+                if (nums[i] > target) {
+                     idx=i;
+                     break;
+                }
+                idx=nums.size();
+            }
+        }
+        return idx;
+    }
 };
